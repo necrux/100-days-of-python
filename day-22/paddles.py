3a2fbb14c1d6ad4a129d3273722a7856413a0a4f
Wes Henderson <whendersonii@gmail.com>
2023-05-14 21:47:27 -0500

    Day 22 complete.

---
+++ day-22/paddles.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.color("white")
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_len= 1, stretch_wid=5)
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
